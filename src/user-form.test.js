815070a39128569b61fb9525d295f02b94c6e527
Martin Zane <martin@zaneweb.com>
2024-05-13 16:30:02 +0200

    Add form submit test

--- src/user-form.test.js
+++ src/user-form.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import UserForm from "./user-form";
+import userEvent from "@testing-library/user-event";
 
 test("it shows two inputs and a button", () => {
   render(<UserForm />);
@@ -9,4 +10,23 @@ test("it shows two inputs and a button", () => {
 
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", () => {
+  const mock = jest.fn();
+  render(<UserForm onUserAdd={mock} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  userEvent.click(nameInput);
+  userEvent.keyboard("jane");
+
+  userEvent.click(emailInput);
+  userEvent.keyboard("jane@jane.com");
+
+  userEvent.click(button);
+
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
 });

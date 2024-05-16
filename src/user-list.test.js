117bc14ebfd1acb0c55d6d2df7ca784b9b293c80
Martin Zane <martin@zaneweb.com>
2024-05-16 08:48:34 +0200

    Add render test for user list

--- src/user-list.test.js
+++ src/user-list.test.js
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import UserList from "./user-list";
 
-test("render one row per user", () => {
+function renderComponent() {
   const users = [
     {
       name: "jane",
@@ -12,12 +12,27 @@ test("render one row per user", () => {
       email: "sam@sam.com",
     },
   ];
-
   const { container } = render(<UserList users={users} />);
+
+  return { users, container };
+}
+
+test("render one row per user", () => {
+  const { container } = renderComponent();
   // eslint-disable-next-line
   const rows = container.querySelectorAll("tbody tr");
 
   expect(rows).toHaveLength(2);
 });
 
-test("render the name and email of each user", () => {});
+test("render the name and email of each user", () => {
+  const { users } = renderComponent();
+
+  users.forEach((user) => {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
+});

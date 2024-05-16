3cdf9688be4c304ca03dfeb16c1aed593392e448
Martin Zane <martin@zaneweb.com>
2024-05-16 09:05:59 +0200

    Test if adding a user through UserForm results in rendered user in the list

---
+++ src/app.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+
+test("can receive a new user and show it on a list", async () => {
+  render(<App />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  await userEvent.click(nameInput);
+  await userEvent.keyboard("jane");
+  await userEvent.click(emailInput);
+  await userEvent.keyboard("jane@jane.com");
+  await userEvent.click(button);
+
+  const name = screen.getByRole("cell", { name: "jane" });
+  const email = screen.getByRole("cell", { name: "jane@jane.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+});
